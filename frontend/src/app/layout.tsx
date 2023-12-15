e5c80a3ef1120eb12e5b8761c3c6b5d406e8a7e0
Rui <rzhengphy@gmail.com>
2023-12-16 01:19:13 +0800

    fix: default jotai store has issue with nextjs

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AuthPanel from "@/components/AuthPanel";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "jotai";
 
 export const metadata: Metadata = {
   title: "UIBBX - UI百宝箱",
@@ -20,13 +21,15 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <QueryProvider>
-          <main>
-            <Header />
-            <AuthPanel />
-            {children}
-            <Footer />
-            <Toaster />
-          </main>
+          <Provider>
+            <main>
+              <Header />
+              <AuthPanel />
+              {children}
+              <Footer />
+              <Toaster />
+            </main>
+          </Provider>
         </QueryProvider>
       </body>
     </html>
